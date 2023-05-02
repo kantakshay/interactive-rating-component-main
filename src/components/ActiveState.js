7e8a1fc0a5d191159d2d0a2435bd180a79efb362
Akshay <akshaykant5006@gmail.com>
2023-05-02 13:33:25 +0530

    functionality added

--- src/components/ActiveState.js
+++ src/components/ActiveState.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import starIcon from "../../images/icon-star.svg";
+import thankIcon from "../../images/illustration-thank-you.svg";
 
 function ActiveState() {
   const [rating, setRating] = useState("");
-//   const [isSubmit, setIsSubmit] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
 
   function handelRatingClicked(items) {
     setRating(items);
   }
-//   function handelSubmit(){
-//     setIsSubmit(true)
-//   }
- 
-  console.log(rating);
-  return (
+
+
+  return (isSubmit === false ?  
     <div className="container">
       
         <div className="icon-container">
@@ -28,11 +26,11 @@ function ActiveState() {
         <div className="user-response">
           <ul className="rating_btn_list">
             {[1, 2, 3, 4, 5].map((items, index) => (
-              <li>
+              <li key={index}>
                 <button
                   className="rating_btn"
                   onClick={() => handelRatingClicked(items)}
-                  key={index}
+              
                 >
                   {items}
                 </button>
@@ -40,12 +38,28 @@ function ActiveState() {
             ))}
           </ul>
 
-          <button className="submit_btn" >
+          <button className="submit_btn" onClick={()=>{ setIsSubmit(true)}} >
             Submit
           </button>
         </div>
      
     </div>
+  :   <div className="container container-thanks_state">
+  {/*  Thank you state start  */}
+  <div>
+    <img src={thankIcon} alt="" />
+  </div>
+
+  <p className="paragraph select-display">You selected {rating} out of 5</p>
+
+  <h1 className="heading">Thank you!</h1>
+  <p className="paragraph">
+    We appreciate you taking the time to give a rating. If you ever need
+    more support, don't hesitate to get in touch!
+  </p>
+
+  {/*  Thank you state end  */}
+</div>
   );
 }
 
